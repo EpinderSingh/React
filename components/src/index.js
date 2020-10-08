9ecf70674396f8d6196a429857f79f45253714b1
Epinder <epinder.singh@gmail.com>
2020-10-08 08:27:41 +0800

    Class base Components

--- components/src/index.js
+++ components/src/index.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Ken"
+          timeAgo="Today at 2:00PM"
+          content="I like the Subject"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Jay"
+          timeAgo="Today at 6:45PM"
+          content="I like the writer."
+        />
+      </ApprovalCard>
     </div>
   );
 };
