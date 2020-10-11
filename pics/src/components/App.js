de1d212b0bb0a325277b58d8146f2362fd41441e
Epinder <epinder.singh@gmail.com>
2020-10-11 18:30:34 +0800

    axios request

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
 
 class App extends React.Component  {
   onSearchSubmit(term){
-    console.log(term)
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {query : term },
+      headers: {
+        Authorization: 'Client-ID kQ1gZWwhlIfwAb4suwMSjtC0M61zoYdLtsSYf2xOrkI'
+      }
+    })
   }
 
   render(){
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <SearchBar runMeWhenUserSubmit={this.onSearchSubmit}/>
       </div>
     );
   }
 }
 
 export default App;
+
+// Access Key
+// kQ1gZWwhlIfwAb4suwMSjtC0M61zoYdLtsSYf2xOrkI
+
+// Secret Key
+// gOv4MB2kFVFBkHK42HwME7kCo09Mgka3ALmqgeri3Pc
