5d0d22d3f1274cf4cd3c7a72f3fa2fe092beb0ce
Epinder <epinder.singh@gmail.com>
2020-10-12 12:43:59 +0800

    this is trouble maker hard to understand

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,19 +4,24 @@ import SearchBar from './SearchBar';
 
 
 class App extends React.Component  {
-  onSearchSubmit(term){
-    axios.get('https://api.unsplash.com/search/photos', {
+  state = { images: [] }
+
+  async onSearchSubmit(term){
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: {query : term },
       headers: {
         Authorization: 'Client-ID kQ1gZWwhlIfwAb4suwMSjtC0M61zoYdLtsSYf2xOrkI'
       }
-    })
+    });
+
+    this.setState({ images: response.data.results });
   }
 
   render(){
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar runMeWhenUserSubmit={this.onSearchSubmit}/>
+        Found: { this.state.images.length } images
       </div>
     );
   }
