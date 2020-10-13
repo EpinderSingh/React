86dcce25cf3d3d4ddddb0e7384f533f5d73dd4f1
Epinder <epinder.singh@gmail.com>
2020-10-13 11:49:37 +0800

    completed pics app

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -19,7 +19,7 @@ class App extends React.Component  {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar runMeWhenUserSubmit={this.onSearchSubmit}/>
-        <ImageList />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
