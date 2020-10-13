022a88253215a2ff3a22066f5636ce140b4ca6c8
Epinder <epinder.singh@gmail.com>
2020-10-13 13:36:24 +0800

    Youtube clone start

---
+++ videos/src/apis/youtube.js
@@ -0,0 +1,1 @@
+const KEY = 'AIzaSyDdcpckMiW2VOTO8g-lG0eJTDG2MeW_OTE';
---
+++ videos/src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { myInput: '' };
+
+  onInputChange = (e) => {
+    this.setState({ myInput: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="feild">
+            <label> Video Search </label>
+            <input
+              onChange={this.onInputChange}
+              type="text"
+              value={this.state.myInput}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
