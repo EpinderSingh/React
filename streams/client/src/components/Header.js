4a985f0dc992c9c8c7d6e43a6571df966479716b
Epinder <epinder.singh@gmail.com>
2020-11-03 08:54:28 +0800

    React authentication with google

---
+++ streams/client/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
+
+const Header = () => {
+  return (
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        Streamy
+      </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          All Streams
+        </Link>
+        <GoogleAuth />
+      </div>
+    </div>
+  );
+};
+export default Header;
+
+// Client ID
+// 949087157826-qes6d2l6jvgmthbjcanhot5sihak8oh3.apps.googleusercontent.com
