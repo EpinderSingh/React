66e95078f03d36b38c09aa53fc8c7345d6164501
Epinder <epinder.singh@gmail.com>
2020-11-16 12:53:57 +0800

    Context

---
+++ translate/src/components/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+
+class App extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag au"
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className="flag in"
+            onClick={() => this.onLanguageChange('punjabi')}
+          />
+        </div>
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
